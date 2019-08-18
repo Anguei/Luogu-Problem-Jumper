dfe321891eed5651db0589f0f4b7a815ff1e962a
ouuan <y___o___u@126.com>
2019-08-19 02:07:43 +0800

    add dblclick listener; optimize problemid check

--- jump.js
+++ jump.js
@@ -1,37 +1,35 @@
 // ==UserScript==
 // @name         Luogu Problem Jumper
-// @version      1.1.1
+// @version      1.2.0
 // @description  双击题号，自动跳转
 // @author       Anguei
 // @match        https://www.luogu.org/*
 // @match        https://*.blog.luogu.org/*
+// @require      https://cdn.bootcss.com/jquery/3.4.1/jquery.min.js
 // @grant        none
 // @namespace    Anguei
 // ==/UserScript==
 
-function jump() {
-    var selection = window.getSelection();
-    var selected = selection.toString();
-    selected = selected.replace(' ', '').toUpperCase();
-    // console.log(selected);
-    if (isProblemId(selected)) {
-        window.open('https://www.luogu.org/problem/' + selected);
-        selection.removeAllRanges();
-    }
+$(document).ready(function() {
+    $(document).dblclick(function() {
+        var selection = window.getSelection();
+        var selected = selection.toString();
+        selected = selected.replace(' ', '').toUpperCase();
+        console.log(selected);
+        if (isProblemId(selected)) {
+            window.open('https://www.luogu.org/problem/' + selected);
+            selection.removeAllRanges();
+        }
 
-    function isProblemId(text) {
-        if (text.length > 8) return false;
-        if (text.match(/AT[0-9]+/) == text) return true;
-        if (text.match(/CF[0-9]+[A-Z][0-9]*/) == text) return true;
-        if (text.match(/SP[0-9]+/) == text) return true;
-        if (text.match(/P[0-9]+/) == text) return true;
-        if (text.match(/UVA[0-9]+/) == text) return true;
-        if (text.match(/U[0-9]+/) == text) return true;
-        if (text.match(/T[0-9]+/) == text) return true;
-        return false;
-    }
-}
-
-var code = jump.toString().split('\n').slice(1, -1).join('\n');
-
-setInterval(code, 200);
+        function isProblemId(text) {
+            if (text.match(/AT[0-9]{1,4}/) == text) return true;
+            if (text.match(/CF[0-9]{1,4}[A-Z][0-9]{0,1}/) == text) return true;
+            if (text.match(/SP[0-9]{1,5}/) == text) return true;
+            if (text.match(/P[0-9]{4}/) == text) return true;
+            if (text.match(/UVA[0-9]{1,5}/) == text) return true;
+            if (text.match(/U[0-9]{1,6}/) == text) return true;
+            if (text.match(/T[0-9]{1,6}/) == text) return true;
+            return false;
+        }
+    });
+});
