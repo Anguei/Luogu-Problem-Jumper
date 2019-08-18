d8023ab4a8ea1bd39ae5e9ad4b949c290de840a8
Anguei <Anguei@users.noreply.github.com>
2019-08-19 00:02:14 +0800

    Update jump.js

--- jump.js
+++ jump.js
@@ -10,24 +10,26 @@
 // ==/UserScript==
 
 function jump() {
-  var selection = window.getSelection();
-  var selected = selection.toString();
-  seleced = selected.replace(' ', '')
-  // console.log(selected);
-  if (isProblemId(selected)) {
-      window.open('https://www.luogu.org/problem/' + selected);
-      selection.removeAllRanges();
-  }
+    var selection = window.getSelection();
+    var selected = selection.toString();
+    seleced = selected.replace(' ', '').toUpperCase();
+    // console.log(selected);
+    if (isProblemId(selected)) {
+        window.open('https://www.luogu.org/problem/' + selected);
+        selection.removeAllRanges();
+    }
 
-  function isProblemId(text) {
-      if (text.length > 8) return false;
-      if (text.match(/AT[0-9]+/) == text) return true;
-      if (text.match(/CF[0-9]+[A-Z][0-9]*/) == text) return true;
-      if (text.match(/SP[0-9]+/) == text) return true;
-      if (text.match(/P[0-9]+/) == text) return true;
-      if (text.match(/UVA[0-9]+/) == text) return true;
-      return false;
-  }
+    function isProblemId(text) {
+        if (text.length > 8) return false;
+        if (text.match(/AT[0-9]+/) == text) return true;
+        if (text.match(/CF[0-9]+[A-Z][0-9]*/) == text) return true;
+        if (text.match(/SP[0-9]+/) == text) return true;
+        if (text.match(/P[0-9]+/) == text) return true;
+        if (text.match(/UVA[0-9]+/) == text) return true;
+        if (text.match(/U[0-9]+/) == text) return true;
+        if (text.match(/T[0-9]+/) == text) return true;
+        return false;
+    }
 }
 
 var code = jump.toString().split('\n').slice(1, -1).join('\n');
