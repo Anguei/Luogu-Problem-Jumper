5b96ca5bc1b99b9108944b85339fc5bbd6854c25
Anguei <Anguei@users.noreply.github.com>
2019-08-19 10:42:39 +0800

    移动端适配

--- jump.js
+++ jump.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Luogu Problem Jumper
-// @version      1.4
+// @version      1.4.1
 // @description  双击题号，自动跳转
 // @author       Anguei, ouuan, abc1763613206
 // @match        https://www.luogu.org/*
@@ -9,12 +9,23 @@
 // @namespace    Anguei
 // ==/UserScript==
 
+function isProblemId(text) {
+  if (text.match(/AT[0-9]{1,4}/) == text) return true;
+  if (text.match(/CF[0-9]{1,4}[A-Z][0-9]{0,1}/) == text) return true;
+  if (text.match(/SP[0-9]{1,5}/) == text) return true;
+  if (text.match(/P[0-9]{4}/) == text) return true;
+  if (text.match(/UVA[0-9]{1,5}/) == text) return true;
+  if (text.match(/U[0-9]{1,6}/) == text) return true;
+  if (text.match(/T[0-9]{1,6}/) == text) return true;
+  return false;
+}
+
 function jump() {
   var selection = window.getSelection();
   var selected = selection.toString().replace(' ', '').toUpperCase();
   var url;
 
-  if(event.ctrlKey) {
+  if (event.ctrlKey) {
     var myBlog = document.querySelectorAll('.ops>a[href*=blog]')[0];
     url = myBlog.href + 'solution-';
   } else {
@@ -24,26 +35,35 @@ function jump() {
   if (isProblemId(selected)) {
     window.open(url + selected);
   }
+}
 
-  function isProblemId(text) {
-    if (text.match(/AT[0-9]{1,4}/) == text) return true;
-    if (text.match(/CF[0-9]{1,4}[A-Z][0-9]{0,1}/) == text) return true;
-    if (text.match(/SP[0-9]{1,5}/) == text) return true;
-    if (text.match(/P[0-9]{4}/) == text) return true;
-    if (text.match(/UVA[0-9]{1,5}/) == text) return true;
-    if (text.match(/U[0-9]{1,6}/) == text) return true;
-    if (text.match(/T[0-9]{1,6}/) == text) return true;
-    return false;
+function jumpMobile() {
+  var selection = window.getSelection();
+  var selected = selection.toString().replace(' ', '').toUpperCase();
+  var url = window.location.href;
+
+  if (isProblemId(selected)) {
+    if (url.match(/message/) == undefined) {
+      window.open('https://www.luogu.org/problem/' + selected);
+    } else {
+      window.open(
+          selection.anchorNode.data.match(/评论了您的博客/) !=
+                  undefined ?
+              'https://' + url.match(/uid=([0-9]+)/)[1] +
+                  '.blog.luogu.org/solution-' + selected :
+              'https://www.luogu.org/problem/' + selected);
+    }
   }
 }
 
 function checkMobile() {
   var ua = navigator.userAgent;
-  return window.screen.width / window.screen.height < 0.6 || ua.indexOf('Android') > -1;
+  return window.screen.width / window.screen.height < 0.6 ||
+      ua.indexOf('Android') > -1;
 }
 
 if (checkMobile()) {
-  document.addEventListener('selectionchange', jump);
+  document.addEventListener('selectionchange', jumpMobile);
 } else {
   document.ondblclick = jump;
 }
